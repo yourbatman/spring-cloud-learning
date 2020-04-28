e99a1f4bcbbc451b264b796fd404c83554506ca8
fangshixiang <fangshixiang@vipkid.com>
2020-04-28 21:48:10 +0800

    init...

--- spring-cloud-context/src/main/java/com/yourbatman/springcloudcontext/Application.java
+++ spring-cloud-context/src/main/java/com/yourbatman/springcloudcontext/Application.java
@@ -2,11 +2,29 @@ package com.yourbatman.springcloudcontext;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext bootContext = SpringApplication.run(Application.class, args);
+        System.out.println("boot容器类型" + bootContext.getClass());
+        System.out.println("boot容器Bean定义总数：" + bootContext.getBeanFactory().getBeanDefinitionCount());
+        System.out.println("boot容器Bean实例总数：" + bootContext.getBeanFactory().getSingletonCount());
+
+
+        // 寻找出BootstrapMarkerConfiguration这个Bean
+        for (String beanDefinitionName : bootContext.getBeanFactory().getSingletonNames()) {
+            // if(beanDefinitionName.contains("bootstrapMarkerConfiguration")){
+                System.out.println(beanDefinitionName);
+            // }
+        }
+
+        ConfigurableApplicationContext cloudContext = (ConfigurableApplicationContext) bootContext.getParent();
+        System.out.println("cloud容器类型" + cloudContext.getClass());
+        System.out.println("cloud容器Bean定义总数：" + cloudContext.getBeanFactory().getBeanDefinitionCount());
+        System.out.println("cloud容器Bean实例总数：" + cloudContext.getBeanFactory().getSingletonCount());
     }
 }
---
+++ spring-cloud-context/src/main/java/com/yourbatman/test/Test1.java
@@ -0,0 +1,12 @@
+package com.yourbatman.test;
+
+import org.junit.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1() {
+        int i = (1 << 4) * 10;
+        System.out.println(i);
+    }
+}
